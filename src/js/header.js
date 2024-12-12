120040a0f4fe2c7906bc4e1187c7a3e5ae186c1d
a.galeev <a.galeev@malltech-lab.ru>
2024-12-12 23:40:04 +0500

    add submenu

--- src/js/header.js
+++ src/js/header.js
@@ -8,6 +8,7 @@ const headerBtn = document.querySelector('.header__btn')
 const headerMainMnu = document.querySelector('.header__main-mnu')
 const headerSubMnu = document.querySelector('.header__sub-mnu')
 const asideMobileBody = document.querySelector('.aside-mobile__body')
+const subMenu = document.querySelector('.sub-mnu')
 
 function hideAside() {
   bodyAside.classList.remove('show')
@@ -29,17 +30,23 @@ function moveElemInAside() {
 
 if (burgerBtn) {
   burgerBtn.addEventListener('click', () => {
-    if ((window.innerWidth <= 900)) {
-      bodyAside.classList.add('show')
-      overlayAside.classList.remove('hidden')
+    // if ((window.innerWidth <= 900)) {
+    //   bodyAside.classList.add('show')
+    //   overlayAside.classList.remove('hidden')
+    //
+    //   moveElemInAside()
+    // } else {
+    //   if (headerSubMnu.classList.contains('open')) {
+    //     headerSubMnu.classList.remove('open')
+    //   } else {
+    //     headerSubMnu.classList.add('open')
+    //   }
+    // }
 
-      moveElemInAside()
+    if (headerSubMnu.classList.contains('open')) {
+      headerSubMnu.classList.remove('open')
     } else {
-      if (headerSubMnu.classList.contains('open')) {
-        headerSubMnu.classList.remove('open')
-      } else {
-        headerSubMnu.classList.add('open')
-      }
+      headerSubMnu.classList.add('open')
     }
 
     if (burgerBtn.classList.contains('open')) {
@@ -47,7 +54,6 @@ if (burgerBtn) {
     } else {
       burgerBtn.classList.add('open')
     }
-
   })
 
   closeAside.addEventListener('click', () => {
@@ -56,6 +62,22 @@ if (burgerBtn) {
 
   overlayAside.addEventListener('click', () => {
     hideAside()
+  })
+}
+
+if (subMenu) {
+  subMenu.addEventListener('click', (event) => {
+    const el = event.target
+
+    if (el.classList.contains('sub-mnu__item-arrow')) {
+      const subMenuItem = el.parentNode
+      if (subMenuItem.classList.contains('open')) {
+        subMenuItem.classList.remove('open')
+      } else {
+        subMenuItem.classList.add('open')
+      }
+    }
+    // if (!event.target.matches('.click-me')) return;
   })
 }
 
